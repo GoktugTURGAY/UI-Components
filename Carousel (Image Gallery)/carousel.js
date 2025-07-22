bc9022a138dc5b36eb8c0ccb5043e139e6d9b0a8
Goktug Turgay <goktug-51@hotmail.com>
2025-07-22 16:36:17 +0300

    Update carousel.js

--- Carousel (Image Gallery)/carousel.js
+++ Carousel (Image Gallery)/carousel.js
@@ -1,11 +1,16 @@
 const figures = document.querySelectorAll(".figure");
+let clickActive = true;
 const removeActiveClasses = () => {
   figures.forEach((figure) => figure.classList.remove("active"));
 };
 
 figures.forEach((figure) => {
   figure.addEventListener("click", () => {
-    removeActiveClasses();
-    figure.classList.add("active");
+    if (clickActive) {
+      removeActiveClasses();
+      figure.classList.add("active");
+      clickActive = false;
+      setTimeout(() => clickActive = true, 1000);
+    }
   });
 });
